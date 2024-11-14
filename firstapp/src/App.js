98194106a8dba8ba0a7221cfc525d5b96d484bf4
Ram13701 <ramanji13701@gmail.com>
2024-11-14 12:07:10 +0530

    commit

--- firstapp/src/App.js
+++ firstapp/src/App.js
@@ -17,7 +17,24 @@ import '../node_modules/bootstrap/dist/css/bootstrap.css'
     <>
    
     <h1>Hello</h1>
-     {state.map((list,index)=><li key={index}>{list.title}</li>)}
+    <div className='container'>
+      <div className='row'>
+        {state.map((list)=>
+        <div className='col-md-4'>
+          <div className="card">
+            <img src={list.img} className="card-img-top" alt="..."/>
+  <div className="card-body">
+    <h5 className="card-title">{list.price}</h5>
+    <h3 className="card-text">{list.title}</h3>
+  </div>
+</div> 
+        </div>
+      )}
+      </div>
+    </div>
+
+
+     {/* {state.map((list,index)=><li key={index}>{list.title}</li>)} */}
 
     </>
   )
